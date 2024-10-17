a49f3612a125af500d6554f1db28a681daa6bbbc
huyluu200202 <125843555+huyluu200202@users.noreply.github.com>
2024-10-17 16:49:48 +0700

    Update index.js

--- backend/db/index.js
+++ backend/db/index.js
@@ -2,7 +2,7 @@ const moongoose = require('mongoose');
 
 async function connect() {
     try {
-        await moongoose.connect('mongodb://localhost:27017/crudDB1');
+        await moongoose.connect('mongodb://localhost:27017/crudDB');
         console.log('Connection successfully');
     } catch (error) {
         console.log(error);
